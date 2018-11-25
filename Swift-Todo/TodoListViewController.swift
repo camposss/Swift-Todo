835506a62cb1e899b350b3bf9572729d5b1e454b
camposss <ccamposs23@gmail.com>
2018-11-25 09:04:47 -0800

    Add new item functionality on UI

--- Swift-Todo/TodoListViewController.swift
+++ Swift-Todo/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Milk",  "get something", "get something else"]
+    var itemArray = ["Find Milk",  "get something", "get something else"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,7 +35,23 @@ class TodoListViewController: UITableViewController {
         
     }
     
-    
+    // MARK - add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once user clicks add item button on UI Alert --> Add to item array
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
     
 }
 
